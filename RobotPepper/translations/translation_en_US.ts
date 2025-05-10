66d911ded79a84b1166028af224f721b42d2dc3d
sofiaouldammar19 <ouldammarsofia@gmail.com>
2025-05-10 21:47:40 +0200

    le comportement du robot avec le fichier du projet pml

---
+++ RobotPepper/translations/translation_en_US.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Bonjour ok</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonjour ok</translation>
+        </message>
+        <message>
+            <source>Bonjour je suis là pour vous aider</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonjour je suis là pour vous aider</translation>
+        </message>
+    </context>
+</TS>
